d5a12c61205e5f92a2e273f6e90502d9c0455b5f
Tanya <94377598+learnertanya@users.noreply.github.com>
2023-10-26 13:02:06 +0530

    second commit

--- file.js
+++ file.js
@@ -1,4 +1,24 @@
 const fs= require("fs");
+const os =require("os");
 fs.writeFileSync("./test.txt","Hey there")
 fs.writeFile("./test2.txt","Asynchronous",(err)=>{})
- 
+const result = fs.readFileSync("./test.txt","utf-8")
+console.log(result)
+//async doestn't return any value,we cannot assign variable to async  readFile
+fs.readFile("./test2.txt","utf-8",(err,result)=>{
+    if(err){
+        console.log("error",err)
+    }
+    else{
+        console.log(result);
+    }
+});
+fs.appendFileSync("./test.txt",`hey there\n`)
+//to copy a file
+fs.cpSync("./test.txt","./copy.txt")
+//to delete a file
+fs.unlinkSync("./copy.txt")
+
+//details of the file
+console.log(fs.statSync("./test.txt"))
+console.log(os.cpus().length);
